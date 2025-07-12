3fc7941008b2e3bed7e9c0f1e1ca7b714e9eeafe
Kirill Gasan <dp160190gkj@gmail.com>
2025-07-13 00:24:05 +0300

    make build

---
+++ index.18135474.js
@@ -0,0 +1,2 @@
+window.addEventListener("resize",function(){window.innerWidth>=1200&&(window.location.hash="")});
+//# sourceMappingURL=index.18135474.js.map
---
+++ index.460be580.js
@@ -0,0 +1,2 @@
+window.addEventListener("resize",function(){window.innerWidth>=1200&&(window.location.hash="")});
+//# sourceMappingURL=index.460be580.js.map
